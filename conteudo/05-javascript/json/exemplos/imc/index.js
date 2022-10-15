70986896bf9ae9e631ab8536e61e928376192ab7
Eduardo Ono <eduardo.ono@gmail.com>
2022-10-14 23:52:29 -0300

    conteudo/05-javascript/ atualizado

---
+++ conteudo/05-javascript/json/exemplos/imc/index.js
@@ -0,0 +1,28 @@
+
+const myList = document.querySelector("ul");
+
+fetch("./pacientes.json")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error, status = ${response.status}`);
+    }
+    return response.json();
+  })
+  .then((data) => {
+    for (const paciente of data.pacientes) {
+      const listItem = document.createElement("li");
+      const nome = document.createElement("strong");
+      nome.textContent = paciente.nome;
+      const peso = document.createElement("strong");
+      peso.textContent = `${paciente.peso}`;
+      const altura = document.createElement("strong");
+      altura.textContent = `${paciente.altura}`;
+      listItem.append(`${nome.textContent}, ${peso.textContent} kg, ${altura.textContent} m`);
+      myList.appendChild(listItem);
+    }
+  })
+  .catch((error) => {
+    const p = document.createElement('p');
+    p.appendChild(document.createTextNode(`Error: ${error.message}`));
+    document.body.insertBefore(p, myList);
+  });
